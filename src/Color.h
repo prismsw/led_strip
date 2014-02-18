182deff451bf0db852ce73e45beec1833c0ef4e4
Simon Strassl <simon.strassl@gmail.com>
2014-02-18 17:32:42 +0100

    Fix indendation

--- src/Color.h
+++ src/Color.h
@@ -1,38 +1,41 @@
 class Color {
-  private:
+private:
     int r;
     int g;
     int b;
-    
+
     double h;
     double s;
     double v;
-    
+
     int boundedRGB(int);
     void updateHSV();
     void updateRGB();
-  public:
+public:
     Color(int, int, int);
-  
+
+    // RGB functions
     int getR();
     int getG();
     int getB();
-    
+
     void setR(int);
     void setG(int);
     void setB(int);
-    
-    void addColor(Color*);
-    void subtractColor(Color*);
-    
+
+    void incR(int);
+    void incG(int);
+    void incB(int);
+
+    // HSV functions
     double getH();
     double getS();
     double getV();
-    
+
     void setH(double);
     void setS(double);
     void setV(double);
-    
+
     void incH(double);
     void incS(double);
     void incV(double);
--- src/led_strip.ino
+++ src/led_strip.ino
@@ -1,6 +1,6 @@
 #include "Color.h"
 
-Color* color = new Color(255, 0, 0);
+Color* color = new Color(0, 0, 0);
 Color* targetColor = new Color(0, 0, 0);
 
 const int rled = 2;
@@ -19,104 +19,106 @@ int heartState = LOW;
 long lastBeat = 0;
 
 void setup() {
-  pinMode(rled, OUTPUT);
-  pinMode(gled, OUTPUT);
-  pinMode(bled, OUTPUT);
+    pinMode(rled, OUTPUT);
+    pinMode(gled, OUTPUT);
+    pinMode(bled, OUTPUT);
 
-  pinMode(heart, OUTPUT);
-  
-  Serial.begin(9600);
+    pinMode(heart, OUTPUT);
+
+    Serial.begin(9600);
 }
 
 void loop() {
-  processIO();
-  setColor(color);
-  digitalWrite(heart, heartState);
-  
-  if(shouldTick(lastBeat, pulse)){
-    beat();
-    lastBeat = millis();
-  }
-  
-  if(shouldTick(lastUpdate, interval)) {
-    switch(mode) {
-      case 1:
-        strip();
-        break;
+    processIO();
+    setColor(color);
+    digitalWrite(heart, heartState);
+
+    if(shouldTick(lastBeat, pulse)) {
+        beat();
+        lastBeat = millis();
     }
-    lastUpdate = millis();
-  }
+
+    if(shouldTick(lastUpdate, interval)) {
+        switch(mode) {
+        case 1:
+            strip();
+            break;
+        }
+        lastUpdate = millis();
+    }
 }
 
 void processIO() {
- while(Serial.available() > 0) {
-    String cmd = Serial.readStringUntil(' ');
-    
-    if(cmd == "rgb") {
-      color->setR(Serial.parseInt());
-      color->setG(Serial.parseInt());
-      color->setB(Serial.parseInt());
+    while(Serial.available() > 0) {
+        String cmd = Serial.readStringUntil(' ');
+
+        if(cmd == "rgb") {
+            color->setR(Serial.parseInt());
+            color->setG(Serial.parseInt());
+            color->setB(Serial.parseInt());
+        }
+        else if(cmd == "fade") {
+            mode = 1;
+            fade = Serial.parseFloat();
+        }
+        else if(cmd == "nofade") {
+            mode = 0;
+        }
+        else if(cmd == "hsv") {
+            color->setH(Serial.parseFloat());
+            color->setS(Serial.parseFloat());
+            color->setV(Serial.parseFloat());
+        }
+        else if(cmd == "h") {
+            color->setH(Serial.parseFloat());
+        }
+        else if(cmd == "s") {
+            color->setS(Serial.parseFloat());
+        }
+        else if(cmd == "v") {
+            color->setV(Serial.parseFloat());
+        }
+        else if(cmd == "print") {
+            Serial.println(color->getR());
+            Serial.println(color->getG());
+            Serial.println(color->getB());
+            Serial.println("");
+        }
+        else {
+            Serial.println("Invalid command: " + cmd);
+            return;
+        }
+        Serial.println(cmd);
     }
-    else if(cmd == "fade") {
-      mode = 1;
-      fade = Serial.parseFloat();
-    }
-    else if(cmd == "nofade") {
-      mode = 0; 
-    }
-    else if(cmd == "hsv") {
-      color->setH(Serial.parseFloat());
-      color->setS(Serial.parseFloat());
-      color->setV(Serial.parseFloat());
-    }
-    else if(cmd == "h") {
-      color->setH(Serial.parseFloat());
-    }
-    else if(cmd == "s") {
-      color->setS(Serial.parseFloat());
-    }
-    else if(cmd == "v") {
-      color->setV(Serial.parseFloat());
-    }
-    else if(cmd == "print") {
-      Serial.println(color->getR());
-      Serial.println(color->getG());
-      Serial.println(color->getB());
-      Serial.println("");
-    }
-    else {
-     Serial.println("Invalid command"); 
-    }
-  } 
 }
 
 int setColor(Color* c) {
-  analogWrite(rled, c->getR());
-  analogWrite(gled, c->getG());
-  analogWrite(bled, c->getB());
+    analogWrite(rled, c->getR());
+    analogWrite(gled, c->getG());
+    analogWrite(bled, c->getB());
 }
 
 boolean shouldTick(long last, int interval) {
-  unsigned long time = millis();
-  
-  long diff = time - last;
-  
-  return diff > interval;
+    unsigned long time = millis();
+
+    long diff = time - last;
+
+    return diff > interval;
 }
 
 void strip() {
-  color->incH(fade);
-  if(color->getH() >= 360 || color->getH() <= 0) {
-   fade = -fade; 
-  }
+    color->incH(fade);
+    if(color->getH() >= 360 || color->getH() <= 0) {
+        fade = -fade;
+    }
 }
 
 void beat() {
-  if(heartState == LOW) {
-    heartState = HIGH;
-  }
-  else {
-    heartState = LOW;
-  }
+    if(heartState == LOW) {
+        heartState = HIGH;
+    }
+    else {
+        heartState = LOW;
+    }
 }
 
